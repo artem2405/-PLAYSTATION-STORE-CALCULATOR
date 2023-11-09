0d40f60a31fba3b705c0a2e6bf53785234d7b4e1
artem2405 <artemkhitin24@gmail.com>
2023-11-09 21:02:20 +0300

    ПЕРВЫЙ КОММИТ - 1 версия приложения с исходными ценовыми коэффициентами была создана 07.10.23 - 2 версия имеет пониженные коэффициенты по всем позициям, была создана 07.11.23

---
+++ PS_CALCULATOR/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace PS_CALCULATOR
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.lira_price = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ruble_price = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lira_price
+            // 
+            this.lira_price.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lira_price.Location = new System.Drawing.Point(17, 68);
+            this.lira_price.Name = "lira_price";
+            this.lira_price.Size = new System.Drawing.Size(247, 30);
+            this.lira_price.TabIndex = 0;
+            this.lira_price.TextChanged += new System.EventHandler(this.lira_price_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(526, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ВВЕДИТЕ ЦЕНУ ИГРЫ В ЛИРАХ: ";
+            // 
+            // ruble_price
+            // 
+            this.ruble_price.Enabled = false;
+            this.ruble_price.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ruble_price.Location = new System.Drawing.Point(17, 216);
+            this.ruble_price.Name = "ruble_price";
+            this.ruble_price.Size = new System.Drawing.Size(247, 30);
+            this.ruble_price.TabIndex = 2;
+            this.ruble_price.TextChanged += new System.EventHandler(this.ruble_price_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(279, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ЦЕНА В РУБЛЯХ:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(309, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(374, 375);
+            this.label3.TabIndex = 4;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(732, 473);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ruble_price);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lira_price);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(750, 520);
+            this.MinimumSize = new System.Drawing.Size(750, 520);
+            this.Name = "MainForm";
+            this.Text = "PS STORE CALCULATOR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lira_price;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ruble_price;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PS_CALCULATOR/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PS_CALCULATOR
+{
+    public partial class MainForm : Form
+    {
+        private double RublePrice;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void lira_price_TextChanged(object sender, EventArgs e)
+        {
+            if (lira_price.Text != "")
+            {
+                if (lira_price.Text[lira_price.Text.Length - 1] >= 48
+                && lira_price.Text[lira_price.Text.Length - 1] <= 57)
+                {
+                    double price = Math.Ceiling(double.Parse(lira_price.Text));
+                    if (price < 100) { RublePrice = price * 5.2; }
+                    if (price >= 100 && price <= 698) { RublePrice = Math.Ceiling(price * 1.07) * 4.75; }
+                    if (price >= 699 && price <= 1198) { RublePrice = Math.Ceiling(price * 1.07) * 4.5; }
+                    if (price >= 1199 && price <= 1798) { RublePrice = Math.Ceiling(price * 1.07) * 4.3; }
+                    if (price >= 1799) { RublePrice = Math.Ceiling(price * 1.07) * 4.1; }
+                    RublePrice = Round10(RublePrice);
+                    ruble_price.Text = RublePrice.ToString();
+                }
+                else if (lira_price.Text[lira_price.Text.Length - 1] != 44)
+                { lira_price.Text = lira_price.Text.Substring(0, lira_price.Text.Length - 1); }
+            }
+            else { ruble_price.Text = ""; }
+        }
+
+        private double Round10(double value)
+        {
+            value = value / 10;
+            value = Math.Ceiling(value);
+            value = value * 10;
+            return value;
+        }
+
+        private void ruble_price_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
